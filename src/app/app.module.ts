6ad2545c6498aa8124b94aa787721126e8acc23d
Joaquín Valdivia <quinihz@gmail.com>
2020-09-03 12:36:00 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { GisComponent } from './pages/gis/gis.component';
+import { DevComponent } from './pages/dev/dev.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Slider1Component } from './shared/slider1/slider1.component';
+import { Grid1Component } from './shared/grid1/grid1.component';
+import { Content1Component } from './shared/content1/content1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutusComponent,
+    GisComponent,
+    DevComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    Slider1Component,
+    Grid1Component,
+    Content1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/gis/gis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gis',
+  templateUrl: './gis.component.html',
+  styleUrls: ['./gis.component.css']
+})
+export class GisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
